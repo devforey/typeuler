204158aa1aa6791106483b2a54d187953925e1a7
Mark Christoper Vizcarra <devforey@gmail.com>
2019-04-04 18:55:05 +0800

    Remove redundant folder

--- src/app/type-game/type-game-routing.module.ts
+++ src/app/type-game/type-game-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
-import {TypeGameComponent} from './type-game/type-game.component';
+import {TypeGameComponent} from './type-game.component';
 
 const routes: Routes = [
   {
--- src/app/type-game/type-game.module.ts
+++ src/app/type-game/type-game.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TypeGameRoutingModule } from './type-game-routing.module';
-import { TypeGameComponent } from './type-game/type-game.component';
+import { TypeGameComponent } from './type-game.component';
 import { FormsModule } from '@angular/forms';
 import { TextCompareModule } from '@lib/text-compare/text-compare.module';
 
